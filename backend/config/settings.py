680c0fbe352293471bf856725614e8b2d067afca
saadsteampc-alt <saad.steampc@gmail.com>
2026-03-09 12:47:11 +0000

    db conn

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pharma_db',
+        'USER': 'postgres',
+        'PASSWORD': '2003',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
